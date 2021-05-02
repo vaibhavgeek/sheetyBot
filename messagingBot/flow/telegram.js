02c820bf7d2395df6cf1de71dafb5aac732f7efa
Vaibhav Maheshwari <vaibhav.m@gigindia.in>
2021-05-03 03:17:23 +0530

    messagingBot update 2.0 added checkAcess function in google sheets.

--- messagingBot/flow/telegram.js
+++ messagingBot/flow/telegram.js
@@ -5,40 +5,60 @@ const db = require("./../utils/db.js");
 const msg = require("./../constants");
 
 const createBot = async function (id, message) {
-  if (message.split("spreadsheets/d/").length > 1) {
-    sid = message.split("spreadsheets/d/")[1].split("/")[0];
+  var matches = /\/([\w-_]{15,})\/(.*?gid=(\d+))?/.exec(message);
+
+  if(matches == null || matches == undefined) 
+    return msg.STEP_1_ERROR;
+  
+  let sid =  matches[1];
+  let access = await sheet.checkAccess(sid); 
+  
+  if (matches && access) {
     await db.updateSheet(id, sid);
-    let link = `https://www.docs.google.com/spreadsheets/d/${sid}/`;
     let workSheet = await sheet.copySheet(sid);
-    if (!workSheet) return msg.STEP_2_ERROR;
     await sheet.renameSheet(sid, workSheet["sheetId"]);
     return [
       msg.STEP_3,
-      `Successfully created botsettings sheet for, ${link}`,
+      `Successfully created botsettings worksheet for, ${message}`,
       msg.STEP_4,
     ];
-  } else {
-    return "Please enter a valid Spreadsheet!";
+  } 
+  else if(!access) {
+    return msg.STEP_3_ERROR;
+  }
+  else {
+    return msg.STEP_2_ERROR;
   }
 };
 
 const notifyBotResponse = async function (id, message, itemRecord) {
-  console.log(itemRecord.Attributes.botsettings);
-  if (itemRecord.Attributes.botsettings.length > 0) {
+  let botsettings = itemRecord.Attributes.botsettings;
+  let responseNotification =
+    "No more updates from the sheeet, at this point of time!";
+
+  if (botsettings.length > 0) {
     let botsetting = itemRecord.Attributes.botsettings[0];
-    await sheet.saveResponse(itemRecord.Attributes.sheetId, botsetting.sheetResponse, botsetting.responseCell , message);
-    await sheet.updateLastActivity(itemRecord.Attributes.sheetId, botsetting);    
-    let responseNotification = "No more updates from the sheeet, at this point of time!";
-    let botsettings = itemRecord.Attributes.botsettings;
+    await sheet.saveResponse(
+      itemRecord.Attributes.sheetId,
+      botsetting.sheetResponse,
+      botsetting.responseCell,
+      message
+    );
+    await sheet.updateLastActivity(itemRecord.Attributes.sheetId, botsetting);
     botsettings.shift();
     await db.sendNotification(itemRecord.Attributes.id, botsettings);
-    if(itemRecord.Attributes.botsettings.length > 1){
-      let item = botsettings[0];
-      responseNotification = `Greetings from Sheety Chatbot, this your ${item["frequency"]} question, 
+  }
+
+  if (botsettings.length > 0) {
+    let item = botsettings[0];
+    responseNotification = `Greetings from Sheety Chatbot, this your ${item["frequency"]} question, 
 Question : *${item["question"]}*,
 Your response will update cell _${item["responseCell"]}_ on worksheet _${item["sheetResponse"]}_.`;
-    }
-    return [`Updated ${botsetting.responseCell} on Sheet ${botsetting.sheetResponse}`, responseNotification];
+
+    return [
+      `Updated ${botsetting.responseCell} on Sheet ${botsetting.sheetResponse}`,
+      responseNotification,
+    ];
   } else {
     return msg.DEFAULT_MESSAGE;
   }
@@ -69,13 +89,15 @@ const telegramBot = async function (message, originalApiRequest) {
   }
 };
 
+module.exports = telegramBot;
+
 //This function is used to test the above code and flow
 
 //(async function () {
   //let message = {
     //sender: 545485074,
     //text:
-      //"Please save it to sheet",
+      //"https://docs.google.com/spreadsheets/u/1/d/1gO2rH6waSDVyjJ_Fd3XczHCejLgkAz7-4XGbhzoXtBU/edit#gid=131091788",
     //originalRequest: {
       //update_id: 403385039,
       //message: {
@@ -111,4 +133,3 @@ const telegramBot = async function (message, originalApiRequest) {
   //const botResponse = await telegramBot(message, {});
   //console.log(botResponse);
 //})();
-module.exports = telegramBot;
