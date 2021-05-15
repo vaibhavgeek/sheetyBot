ce9547d34bc5bb7c2253aebe27b0a8db40291b39
Vaibhav Maheshwari <vaibhav.m@gigindia.in>
2021-05-15 19:06:38 +0530

    Update Homepage - Added producthunt button

--- messagingBot/constants.js
+++ messagingBot/constants.js
@@ -6,7 +6,7 @@ const msg = {
   STEP_3: "Please setup botsettings worksheet on your google sheet to proceed",
   STEP_4: "Done!, you will now recieve updates from the chatbot and you can update your google sheet directly from this bot!",
   STEP_1_ERROR: "There was some error while requesting access for your google sheet, please contact at vaibhavblogger@gmail.com for more info",
-  STEP_2_ERROR: "The file you have shared does not grant us the permission to edit it, please update sharing settigs or give proper access",
+  STEP_2_ERROR: "The file you have shared does not grant us the permission to edit it, please update sharing settigs or give proper access, chatbot@edunova-979.iam.gserviceaccount.com",
   STEP_3_ERROR: "The worksheet already exists, please setup botsettings as explained in the instructions found at https://sheety.vacode.xyz ",
   STEP_4_ERROR: "Something went wrong, please contact admin for more support",
   DEFAULT_MESSAGE: "Sorry, could not understand what you said! Apologies for any confusion",
